5a9169272b33e1cddadec72d0b98ca451f72f6b5
Paul Muchiri <58714292+pmuchiri@users.noreply.github.com>
2020-01-10 19:37:09 -0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div :class="classObject">{{ value }}</div>
+    <div :style="{ fontSize: (value * 5) + 'px' }">{{ value }}</div>
+
+    <button type="button" @click="value++">+</button>
+    <button type="button" @click="value--">-</button>
+    <button type="button" @click="error = ! error">Error</button>
+    <button type="button" @click="warning = ! warning">Warning</button>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'app',
+    
+    data: function () {
+      return {
+        value: 1,
+        error: false,
+        warning: false
+      }
+  },
+  computed: {
+    classObject: function () {
+      return { 
+        'red-error' : this.value > 5, 
+        'green-error' : this.value <= 5,
+        'yellow-warning' : this.warning,
+      }
+    }
+  }
+} 
+</script>
+
+<style>
+  body  {
+    font-size: 40px;
+    text-align: center;
+    padding-top: 100px;
+  }
+    .red-error {
+      background-color: darkred;
+    }
+    .green-error {
+      background-color: darkgreen;
+    }
+    .yellow-warning {
+      text-decoration: underline;
+    }
+
+</style>
---
+++ Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+
+    }
+
+</script>
+
+
+<style>
+
+</style>
